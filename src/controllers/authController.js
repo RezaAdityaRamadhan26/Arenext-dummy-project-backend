e9c89d38eff282811555ba0ceda4f837447bd188
faizadie30 <faiz.adie30@gmail.com>
2026-02-27 02:02:52 +0700

    auth: login, register using bcrypt and jwt done!

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
+import jwt  from 'jsonwebtoken';
 import prisma from '../prisma.js';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcrypt';    
 
 export const userRegist = async (req, res) => {
     try {
@@ -42,5 +43,51 @@ export const userRegist = async (req, res) => {
             message: "server error",
             error: error.message
         })
+    }
+}
+
+export const userLogin = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await prisma.user.findUnique({
+            where: {
+                email: email
+            }
+        })
+
+        if (!user) {
+        return res.status(404).json({
+            success: false,
+            message: "email tidak terdaftar",
+        });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            return res.status(401).json({
+                success: false,
+                message: "Password salah!",
+            });
+        }
+
+        const token = jwt.sign(
+            { id: user.id, email: user.email }, 
+            process.env.JWT_SECRET, 
+            { expiresIn: "1d" }, // 1 hari token nya
+        );
+        res.status(200).json({
+            success: true,
+            message: "Login berhasil!",
+            token: token
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Gagal login",
+            error: error.message,
+        });
     }
 }
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { userRegist } from '../controllers/authController.js'
+import { userRegist, userLogin } from '../controllers/authController.js'
 
 const router = express.Router();
-
+    
 router.post('/register', userRegist)
+router.post('/login', userLogin)
 
 export default router;
