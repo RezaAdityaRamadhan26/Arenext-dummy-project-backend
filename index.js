0530c12751dc53e68e94a5643bc2ed14ad336376
reza aditya <rezaadityaa26@gmail.com>
2026-03-31 19:38:27 +0700

    fix cors

--- index.js
+++ index.js
@@ -2,30 +2,31 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import venueRoutes from './src/routes/venueRoutes.js';
-import authRoutes from './src/routes/authRoutes.js' 
-import bookingRoutes  from './src/routes/bookingRoutes.js';
+import authRoutes from './src/routes/authRoutes.js'; 
+import bookingRoutes from './src/routes/bookingRoutes.js';
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000; 
 
-app.use(cors());
+app.use(cors({
+  origin: "*", 
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"] 
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));  
 
 app.use('/public', express.static('public')); 
 
 app.use("/api/venues", venueRoutes);
-app.use("/api/auth", authRoutes)
-app.use("/api/booking", bookingRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/booking", bookingRoutes);
 
 app.get('/', (req, res) => {
   res.send('berhasil!');
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
 });
 
 export default app;
