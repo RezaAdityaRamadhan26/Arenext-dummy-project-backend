2a0d4c2278edc530a52853d265f440f281275fd5
reza aditya <rezaadityaa26@gmail.com>
2026-04-02 23:13:59 +0700

    still fixing admin dashboard

--- src/routes/venueRoutes.js
+++ src/routes/venueRoutes.js
@@ -1,19 +1,21 @@
-import express from 'express';
-import { uploadVenueImage } from '../middlewares/uploadMiddleware.js';
-import {
-  createVenue,
-  getAllVenue,
-  updateVenue,
-  deleteVenue,
-} from "../controllers/venueController.js";
-import { authenticateToken, authorizeAdmin } from '../middlewares/authMiddlewares.js';
+  import express from 'express';
+  import { uploadVenueImage } from '../middlewares/uploadMiddleware.js';
+  import {
+    createVenue,
+    getAllVenue,
+    updateVenue,
+    deleteVenue,
+    getVenueById
+  } from "../controllers/venueController.js";
+  import { authenticateToken, authorizeAdmin } from '../middlewares/authMiddlewares.js';
 
-const router = express.Router();
+  const router = express.Router();
 
-router.get('/:id', getAllVenue)
-router.post('/', authenticateToken, authorizeAdmin, uploadVenueImage.single('image'), createVenue)
+  router.get('/', getAllVenue)
+  router.get('/:id', getVenueById)
+  router.post('/', authenticateToken, authorizeAdmin, uploadVenueImage.single('image'), createVenue)
 
-router.put('/:id', updateVenue)
-router.delete('/:id', deleteVenue)
+  router.put('/:id', authenticateToken, authorizeAdmin, uploadVenueImage.single('image'), updateVenue)
+  router.delete('/:id', authenticateToken, authorizeAdmin, deleteVenue)
 
-export default router;  
+  export default router;  
