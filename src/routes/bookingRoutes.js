983153c4a92d08c593cf05b2b2a17095d944d773
reza aditya <rezaadityaa26@gmail.com>
2026-04-03 00:02:34 +0700

    stil fixing bookings

--- src/routes/bookingRoutes.js
+++ src/routes/bookingRoutes.js
@@ -3,13 +3,15 @@ import express from 'express';
 import {
     createBooking,
     getAllBooking,
-    updateBookingStatus
+    updateBookingStatus,
+    getMyBookings
 } from '../controllers/bookingController.js';
 import { authenticateToken  } from '../middlewares/authMiddlewares.js'
 
 const router = express.Router()
 
 router.get('/', getAllBooking)
+router.get('/my-bookings', authenticateToken, getMyBookings)
 router.post('/', authenticateToken, createBooking)
 router.post('/:id/status', authenticateToken, updateBookingStatus)
 
